c3311bf9ef62a4ded5e961132c2b52ac0539ce15
unknown <inexperiencedme@protonmail.com>
2024-04-18 21:57:25 +0200

    shapes study, will branch off for policy integration

--- trainer.py
+++ trainer.py
@@ -9,16 +9,19 @@ env = UnityInterface(None)
 print(f"{env.getSpecs()}")
 behaviorNames = env.getBehaviorNames()
 
+rewards = {}
+for behavior in behaviorNames:
+    rewards[behavior] = np.zeros((env.getSpecs(behavior)["AgentsCount"]))
 
-totalSteps = 1000
+totalSteps = 10
 for i in range(1, totalSteps+1):
     for behavior in behaviorNames:
         decisionSteps, terminalSteps = env.getSteps(behavior)
         agentsThatRequestAction = list(decisionSteps)
         agentsThatFinished = list(terminalSteps)
-        print(f"Number of agents that request an action {len(decisionSteps)}. Number of agents that finished: {len(terminalSteps)}")
+        # print(f"Agents that request an action {len(decisionSteps)}. Agents that finished: {len(terminalSteps)}")
         
-        behaviorActions = {"continuous": None, "discrete": None}
+        behaviorActions = {"continuous": None, "discrete": None} # This is agent's part
         specs = env.getSpecs(behavior)
         nrOfContinuousActions = specs["ContinuousActions"]
         nrOfDiscreteActions = len(specs["DiscreteActions"])
@@ -27,7 +30,23 @@ for i in range(1, totalSteps+1):
         if nrOfDiscreteActions > 0:
             behaviorActions["discrete"] = np.zeros((len(agentsThatRequestAction), nrOfDiscreteActions), dtype=np.int32)
 
+
+        observations = []
+        for agent in agentsThatRequestAction: # rewards handling
+            rewards[behavior][agent] += decisionSteps[agent].reward 
+            observations.append(decisionSteps[agent].obs)
         
+        obsShapes = []
+        obsDimensionalites = []
+        for element in observations[0]:
+            obsShapes.append(element.shape)
+            obsDimensionalites.append(len(element.shape))
+        print(f"{observations[0]} of shapes {obsShapes}, thus, dimensions {obsDimensionalites}\n\n")
+
+        for agent in agentsThatFinished:
+            rewards[behavior][agent] += terminalSteps[agent].reward
+            print(f"Final reward: {rewards[behavior][agent]:.2f}")
+            rewards[behavior][agent] = 0
 
         env.setActions(behavior, behaviorActions['continuous'], behaviorActions['discrete'])
         env.step()
--- utils.py
+++ utils.py
@@ -55,3 +55,4 @@ class UnityInterface():
 
     def addExperiences(self):
         pass
+
