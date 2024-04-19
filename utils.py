7292b408895e7d8fa5415d7e3321c02ef5bfc7e6
unknown <inexperiencedme@protonmail.com>
2024-04-20 01:39:55 +0200

    PPO with observation and action handling, untested

--- utils.py
+++ utils.py
@@ -69,40 +69,47 @@ def linearInitialize(layer, std=np.sqrt(2), bias_const=0.0):
 class PPO(nn.Module):
     def __init__(self, envSpecs):
         super(PPO, self).__init__()
+        self.envSpecs = envSpecs
+        self.obsSize1D, self.obsSize3D = self.getObsSizes(self.envSpecs)
+        self.obsChannels3D = self.obsSize3D[0] # first shape dim is channels
+        self.continuousActionSize = self.envSpecs["ContinuousActions"]
+
         self.preCritic1D = nn.Sequential(
-            linearInitialize(nn.Linear(obsSize1D, 256)), nn.Tanh(),
+            linearInitialize(nn.Linear(self.obsSize1D, 256)), nn.Tanh(),
             linearInitialize(nn.Linear(256, 128)), nn.Tanh(),
             linearInitialize(nn.Linear(128, 64)), nn.Tanh())
         
         # TODO: Check if 3D obs exist
         self.preCritic3D = nn.Sequential(
-            linearInitialize(nn.Conv2d(obs3Dchannels, 32, 8, stride=4)), nn.Tanh(),
+            linearInitialize(nn.Conv2d(self.obsChannels3D, 32, 8, stride=4)), nn.Tanh(),
             linearInitialize(nn.Conv2d(32, 64, 4, stride=2)), nn.Tanh(),
             linearInitialize(nn.Conv2d(64, 64, 3, stride=1)), nn.Tanh(), nn.Flatten())
+        self.preCritic3DoutputSize = self.calculateConvNetOutputSize(self.preCritic3D, self.obsSize3D)
 
-        self.criticFinal = nn.Sequential(linearInitialize(nn.Linear(64 + preCritic3DoutputSize, 1), std=0.01))
+        self.criticFinal = nn.Sequential(linearInitialize(nn.Linear(64 + self.preCritic3DoutputSize, 1), std=0.01))
 
 
         self.preActor1D = nn.Sequential(
-            linearInitialize(nn.Linear(obsSize1D, 256)), nn.Tanh(),
+            linearInitialize(nn.Linear(self.obsSize1D, 256)), nn.Tanh(),
             linearInitialize(nn.Linear(256, 128)), nn.Tanh(),
             linearInitialize(nn.Linear(128, 64)), nn.Tanh())
         
         self.preActor3D = nn.Sequential(
-            linearInitialize(nn.Conv2d(obs3Dchannels, 32, 8, stride=4)), nn.Tanh(),
+            linearInitialize(nn.Conv2d(self.obsChannels3D, 32, 8, stride=4)), nn.Tanh(),
             linearInitialize(nn.Conv2d(32, 64, 4, stride=2)), nn.Tanh(),
             linearInitialize(nn.Conv2d(64, 64, 3, stride=1)), nn.Tanh(), nn.Flatten())
-            
+        self.preActor3DoutputSize = self.calculateConvNetOutputSize(self.preActor3D, self.obsSize3D)
+
         self.actorContinuous = nn.Sequential(
-            linearInitialize(nn.Linear(64 + preActor3DoutputSize, 64)), nn.Tanh(),
-            linearInitialize(nn.Linear(64, continuousActionSize), std=0.01))
-        self.actorLogStd = nn.Parameter(torch.zeros(continuousActionSize))
+            linearInitialize(nn.Linear(64 + self.preActor3DoutputSize, 64)), nn.Tanh(),
+            linearInitialize(nn.Linear(64, self.continuousActionSize), std=0.01))
+        self.actorLogStd = nn.Parameter(torch.zeros(self.continuousActionSize))
 
         self.actorDiscrete = []
-        for discreteActionSize in discreteBranches:
+        for discreteActionSize in self.envSpecs["DiscreteActions"]:
             self.actorDiscrete.append(nn.Sequential(
-            linearInitialize(nn.Linear(64, 64), nn.Tanh(),
-            linearInitialize(nn.Linear(64, discreteActionSize), std=0.01))))
+            linearInitialize(nn.Linear(64 + self.preActor3DoutputSize, 64)), nn.Tanh(),
+            linearInitialize(nn.Linear(64, discreteActionSize), std=0.01)))
 
     def evaluateState(self, x):
         obs1D, obs3D = self.processObservations(x)
@@ -119,8 +126,8 @@ class PPO(nn.Module):
             
         logProbs = []
         entropies = []
-        for discreteAction in range(discreteActionSize):
-            logits = self.actorDiscrete(observationFeatures)
+        for discreteAction in range(len(self.envSpecs["DiscreteActions"])):
+            logits = self.actorDiscrete[discreteAction](observationFeatures)
             probabilities = Categorical(logits=logits)
             if action is None:
                 actionTemp.append(probabilities.sample())
@@ -144,8 +151,9 @@ class PPO(nn.Module):
         return action, probabilities.log_prob(action).sum(-1), probabilities.entropy().sum(-1), self.critic(x)
 
     def processObservations(self, x):
-        obs1D = torch.zeros((0,))
-        obs3D = torch.zeros((0,))
+        # TODO Getting a warning with this approach.. Could rework
+        obs1D = torch.empty((0,))
+        obs3D = torch.empty((0,))
         for observation in x:
             observation = torch.tensor(observation)
             if len(observation.shape) == 1:
@@ -156,3 +164,24 @@ class PPO(nn.Module):
                 print(f"Unexpected {len(observation.shape)}-dimensional observation")
         # TODO: Put it on device?
         return obs1D, obs3D
+    
+    def getObsSizes(self, specs):
+        obsShape1D = 0
+        obsShape3D = [0, 0, 0]
+        for obsShape in specs["Observations"]:
+            if len(obsShape) == 1:
+                obsShape1D = obsShape1D + obsShape[0]
+            elif len(obsShape) == 3:
+                for i, size in enumerate(obsShape):
+                    obsShape3D[i] = obsShape3D[i] + size
+            else:
+                print(f"Unexpected {len(obsShape)}-dimensional observation")
+        return obsShape1D, obsShape3D
+    
+    def calculateConvNetOutputSize(self, net, inputSize):
+        return torch.numel(torch.flatten(net(torch.ones(inputSize))))
+
+env = UnityInterface(None)
+behaviorNames = env.getBehaviorNames()
+for behavior in behaviorNames:
+    agent = PPO(env.getSpecs(behavior))
