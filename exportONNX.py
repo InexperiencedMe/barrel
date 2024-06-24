f7641ac864b57b26908b49cd0a567f6c256a24d8
unknown <inexperiencedme@protonmail.com>
2024-06-25 00:48:35 +0200

    added 3DBall onnx

--- exportONNX.py
+++ exportONNX.py
@@ -4,16 +4,16 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # TODO: It doesn't support multiple behaviors in one env. Need to find a neat way to load multiple checkpoints. I should save checkpoints with dicts that hold all behaviors in one checkpoint
 
 
-# env = UnityInterface("Builds\\Windows\\Ball3D\\UnityEnvironment")      # 1D obs only, continuous action of size 2. Rewards: 0.1 for every step, -1 for fail, 100 is the max episodic return
+env = UnityInterface("Builds\\Windows\\Ball3D\\UnityEnvironment")      # 1D obs only, continuous action of size 2. Rewards: 0.1 for every step, -1 for fail, 100 is the max episodic return
 # env = UnityInterface("Builds\\Windows\\Crawler\\UnityEnvironment")     # 1D obs only, continuous action of size 8
-env = UnityInterface("Builds\\Windows\\PushBlock\\UnityEnvironment")   # 1D obs only, discrete action of size (7). Rewards: 5 for win, -0.001 for every step
+# env = UnityInterface("Builds\\Windows\\PushBlock\\UnityEnvironment")   # 1D obs only, discrete action of size (7). Rewards: 5 for win, -0.001 for every step
 # env = UnityInterface("Builds\\Windows\\WallJump\\UnityEnvironment")    # 1D obs only, discrete action of size (3, 3, 3, 2)
 # env = UnityInterface(None) 
 
 print(f"{env.getSpecs()}")
 behaviorNames = env.getBehaviorNames()
 
-checkpointNames = ["PushBlock-mod-500000", "PushBlock-mod-550000", "PushBlock-mod-650000", "PushBlock-mod-700000", "PushBlock-mod-750000"]
+checkpointNames = ["3DBall--5000", "3DBall--10000", "3DBall--15000", "3DBall--30000", "3DBall--50000", "3DBall--100000", "3DBall--150000"]
 checkpoints = [f"checkpoints\\{name}.pth" for name in checkpointNames]
 
 for i, checkpoint in enumerate(checkpoints):
